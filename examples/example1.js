8e4c0783537221e453ac35036d91658bcb602836
ApoloGH <apolo-gh@users.noreply.github.com>
2022-10-02 15:44:16 -0400

    test: add unit-tests for examples

---
+++ examples/example1.js
@@ -0,0 +1,15 @@
+import ExtendablePromise from '../index.js';
+
+class MyPromise extends ExtendablePromise {
+  // do whatever you need
+}
+
+const myPromise = new MyPromise((resolve, reject) => {
+  console.log('executed', resolve, reject);
+});
+console.log(myPromise instanceof Promise); // true
+myPromise.execute(); // 'executed' [Function (anonymous)] [Function (anonymous)]
+myPromise.then(result => {
+  console.log(result); // 123
+});
+myPromise.resolve(123);
---
+++ examples/example1.test.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { jest } from '@jest/globals';
+
+describe('Examples', () => {
+  beforeEach(() => {
+    global.console = { log: jest.fn() };
+  });
+  afterEach(() => {
+    jest.resetModules();
+  });
+
+  test('Example 1', async () => {
+    expect.assertions(4);
+    await import('./example1.js');
+    expect(console.log).toHaveBeenCalledTimes(3);
+    expect(console.log.mock.calls[0]).toEqual([true]);
+    expect(console.log.mock.calls[1]).toEqual(['executed', expect.any(Function), expect.any(Function)]);
+    expect(console.log.mock.calls[2]).toEqual([123]);
+  });
+});
